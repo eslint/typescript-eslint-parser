11d55e5ddea5fd4e121e5f5390e499fd9b3f0934
James Henry <james@henry.sc>
2016-10-22 12:10:59 +0100

    Chore: Added test for shorthand-ambient-module-declaration (refs #105)

---
+++ tests/fixtures/typescript/namespaces-and-modules/shorthand-ambient-module-declaration.result.js
@@ -0,0 +1,151 @@
+module.exports = {
+    "body": [
+        {
+            "flags": 2,
+            "loc": {
+                "end": {
+                    "column": 32,
+                    "line": 1
+                },
+                "start": {
+                    "column": 0,
+                    "line": 1
+                }
+            },
+            "modifiers": [
+                {
+                    "flags": 0,
+                    "loc": {
+                        "end": {
+                            "column": 7,
+                            "line": 1
+                        },
+                        "start": {
+                            "column": 0,
+                            "line": 1
+                        }
+                    },
+                    "range": [
+                        0,
+                        7
+                    ],
+                    "type": "TSDeclareKeyword"
+                }
+            ],
+            "name": {
+                "loc": {
+                    "end": {
+                        "column": 31,
+                        "line": 1
+                    },
+                    "start": {
+                        "column": 15,
+                        "line": 1
+                    }
+                },
+                "range": [
+                    15,
+                    31
+                ],
+                "raw": "\"hot-new-module\"",
+                "type": "Literal",
+                "value": "hot-new-module"
+            },
+            "range": [
+                0,
+                32
+            ],
+            "type": "TSModuleDeclaration"
+        }
+    ],
+    "loc": {
+        "end": {
+            "column": 32,
+            "line": 1
+        },
+        "start": {
+            "column": 0,
+            "line": 1
+        }
+    },
+    "range": [
+        0,
+        32
+    ],
+    "sourceType": "script",
+    "tokens": [
+        {
+            "loc": {
+                "end": {
+                    "column": 7,
+                    "line": 1
+                },
+                "start": {
+                    "column": 0,
+                    "line": 1
+                }
+            },
+            "range": [
+                0,
+                7
+            ],
+            "type": "Identifier",
+            "value": "declare"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 14,
+                    "line": 1
+                },
+                "start": {
+                    "column": 8,
+                    "line": 1
+                }
+            },
+            "range": [
+                8,
+                14
+            ],
+            "type": "Identifier",
+            "value": "module"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 31,
+                    "line": 1
+                },
+                "start": {
+                    "column": 15,
+                    "line": 1
+                }
+            },
+            "range": [
+                15,
+                31
+            ],
+            "type": "String",
+            "value": "\"hot-new-module\""
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 32,
+                    "line": 1
+                },
+                "start": {
+                    "column": 31,
+                    "line": 1
+                }
+            },
+            "range": [
+                31,
+                32
+            ],
+            "type": "Punctuator",
+            "value": ";"
+        }
+    ],
+    "type": "Program"
+};
