269b558a5f7f8ef4a7cee899c896ca65931cb21e
Rasmus Eneman <pie.or.paj@gmail.com>
2017-03-19 15:02:00 +0100

    Fix: Label variable declartions as Ambient (fixes #185)

---
+++ tests/fixtures/typescript/basics/declare-variable.result.js
@@ -0,0 +1,220 @@
+module.exports = {
+    "body": [
+        {
+            "declarations": [
+                {
+                    "id": {
+                        "loc": {
+                            "end": {
+                                "column": 13,
+                                "line": 1
+                            },
+                            "start": {
+                                "column": 12,
+                                "line": 1
+                            }
+                        },
+                        "name": "a",
+                        "range": [
+                            12,
+                            13
+                        ],
+                        "type": "Identifier",
+                        "typeAnnotation": {
+                            "loc": {
+                                "end": {
+                                    "column": 21,
+                                    "line": 1
+                                },
+                                "start": {
+                                    "column": 15,
+                                    "line": 1
+                                }
+                            },
+                            "range": [
+                                15,
+                                21
+                            ],
+                            "type": "TypeAnnotation",
+                            "typeAnnotation": {
+                                "loc": {
+                                    "end": {
+                                        "column": 21,
+                                        "line": 1
+                                    },
+                                    "start": {
+                                        "column": 15,
+                                        "line": 1
+                                    }
+                                },
+                                "range": [
+                                    15,
+                                    21
+                                ],
+                                "type": "TSStringKeyword"
+                            }
+                        }
+                    },
+                    "init": null,
+                    "loc": {
+                        "end": {
+                            "column": 21,
+                            "line": 1
+                        },
+                        "start": {
+                            "column": 12,
+                            "line": 1
+                        }
+                    },
+                    "range": [
+                        12,
+                        21
+                    ],
+                    "type": "VariableDeclarator"
+                }
+            ],
+            "kind": "let",
+            "loc": {
+                "end": {
+                    "column": 22,
+                    "line": 1
+                },
+                "start": {
+                    "column": 0,
+                    "line": 1
+                }
+            },
+            "range": [
+                0,
+                22
+            ],
+            "type": "TSAmbientVariableDeclaration"
+        }
+    ],
+    "loc": {
+        "end": {
+            "column": 22,
+            "line": 1
+        },
+        "start": {
+            "column": 0,
+            "line": 1
+        }
+    },
+    "range": [
+        0,
+        22
+    ],
+    "sourceType": "script",
+    "tokens": [
+        {
+            "loc": {
+                "end": {
+                    "column": 7,
+                    "line": 1
+                },
+                "start": {
+                    "column": 0,
+                    "line": 1
+                }
+            },
+            "range": [
+                0,
+                7
+            ],
+            "type": "Identifier",
+            "value": "declare"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 11,
+                    "line": 1
+                },
+                "start": {
+                    "column": 8,
+                    "line": 1
+                }
+            },
+            "range": [
+                8,
+                11
+            ],
+            "type": "Keyword",
+            "value": "let"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 13,
+                    "line": 1
+                },
+                "start": {
+                    "column": 12,
+                    "line": 1
+                }
+            },
+            "range": [
+                12,
+                13
+            ],
+            "type": "Identifier",
+            "value": "a"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 14,
+                    "line": 1
+                },
+                "start": {
+                    "column": 13,
+                    "line": 1
+                }
+            },
+            "range": [
+                13,
+                14
+            ],
+            "type": "Punctuator",
+            "value": ":"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 21,
+                    "line": 1
+                },
+                "start": {
+                    "column": 15,
+                    "line": 1
+                }
+            },
+            "range": [
+                15,
+                21
+            ],
+            "type": "Identifier",
+            "value": "string"
+        },
+        {
+            "loc": {
+                "end": {
+                    "column": 22,
+                    "line": 1
+                },
+                "start": {
+                    "column": 21,
+                    "line": 1
+                }
+            },
+            "range": [
+                21,
+                22
+            ],
+            "type": "Punctuator",
+            "value": ";"
+        }
+    ],
+    "type": "Program"
+};
