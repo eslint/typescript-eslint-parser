80bde1d5c81e022c7cb401ec3cad996d8265824d
Toru Nagashima <star.ctor@gmail.com>
2018-11-20 03:16:04 +0100

    Fix issue with scope analyze found by @mysticatea

--- analyze-scope.js
+++ analyze-scope.js
@@ -69,6 +69,9 @@ class EnumScope extends Scope {
 class PatternVisitor extends OriginalPatternVisitor {
     Identifier(node) {
         super.Identifier(node);
+        if (node.decorators) {
+            this.rightHandNodes.push(...node.decorators);
+        }
         if (node.typeAnnotation) {
             this.rightHandNodes.push(node.typeAnnotation);
         }
@@ -76,6 +79,9 @@ class PatternVisitor extends OriginalPatternVisitor {
 
     ArrayPattern(node) {
         node.elements.forEach(this.visit, this);
+        if (node.decorators) {
+            this.rightHandNodes.push(...node.decorators);
+        }
         if (node.typeAnnotation) {
             this.rightHandNodes.push(node.typeAnnotation);
         }
@@ -83,6 +89,9 @@ class PatternVisitor extends OriginalPatternVisitor {
 
     ObjectPattern(node) {
         node.properties.forEach(this.visit, this);
+        if (node.decorators) {
+            this.rightHandNodes.push(...node.decorators);
+        }
         if (node.typeAnnotation) {
             this.rightHandNodes.push(node.typeAnnotation);
         }
