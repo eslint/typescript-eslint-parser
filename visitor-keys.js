42f1801cdc9e26b271ab8ad7f8399669118ff34b
Armano <armano2@users.noreply.github.com>
2018-12-16 02:14:19 +0100

    Fix: Allow to visit typeParameters in VariableDeclarator

--- visitor-keys.js
+++ visitor-keys.js
@@ -21,6 +21,8 @@ module.exports = Evk.unionWith({
     ObjectPattern: ["properties", "typeAnnotation"],
     NewExpression: ["callee", "typeParameters", "arguments"],
     CallExpression: ["callee", "typeParameters", "arguments"],
+    // typeParameters are preset only when kind='type'
+    VariableDeclarator: ["id", "typeParameters", "init"],
 
     // Additional Nodes.
     ClassProperty: ["decorators", "key", "typeAnnotation", "value"],
